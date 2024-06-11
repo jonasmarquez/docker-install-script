cf7ae0d85b04b52d9a0f2b8abe413723f47246d0
Jonás Márquez <jonas@jonasmarquez.com>
2024-06-11 14:11:14 +0200

    add -y parameter

--- docker/ubuntu_docker_latest.sh
+++ docker/ubuntu_docker_latest.sh
@@ -15,7 +15,7 @@ echo \
 
 # Install Docker Engine
 sudo apt-get update
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
 # MANAGE DOCKER USERS
 
